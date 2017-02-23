e255920ba07801a4a293f6ceea9694027dbc70b7
Adam Ralph <adam@adamralph.com>
2017-02-23 12:50:02 +0100

    refactor: build.cmd whitespace and comments

--- build.cmd
+++ build.cmd
@@ -30,19 +30,23 @@ mkdir artifacts
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "If (Test-Path .\artifacts\files\) { Remove-Item .\artifacts\files\ -Recurse -Force }"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "Get-ChildItem .\artifacts\ -Filter *.nupkg | Select-Object -First 1 | foreach { Copy-Item $_.FullName .\artifacts\files.zip -Force }"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "Expand-Archive .\artifacts\files.zip -DestinationPath .\artifacts\files"
+
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\artifacts\files\internal\runner.csx || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\artifacts\files\internal\target-runner.csx || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\artifacts\files\internal\util.csx || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\artifacts\files\simple-targets-target.csx || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\artifacts\files\simple-targets.csx || goto :error
+
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\test.csx -? || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\test.csx -T || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\test.csx -D || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\test.csx -n || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\test.csx || goto :error
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\test.csx "hell""o" || goto :error
+
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\quickstart.csx || goto :error
 
+:: exit
 goto :EOF
 :error
 exit /b %errorlevel%
