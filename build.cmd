58da7b820b442e792202278f59d0e4abf818a308
Blair Conrad <blair@blairconrad.com>
2017-05-18 07:11:23 -0400

    Test detection of unknown targets

--- build.cmd
+++ build.cmd
@@ -48,6 +48,8 @@ mkdir artifacts
 
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\double-dependency.csx || goto :error
 
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\tests\check-targets-up-front.csx --no-color build notarealtarget || goto :error
+
 :: exit
 goto :EOF
 :error
